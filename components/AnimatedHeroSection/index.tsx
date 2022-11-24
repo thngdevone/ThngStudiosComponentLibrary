e6baf27150a882e7456be35f0af31a9b40c37a0c
thngdevone <118913331+thngdevone@users.noreply.github.com>
2022-11-24 13:17:52 -0500

    Added shadow to triangle + allowed for custom time interval

--- components/AnimatedHeroSection/index.tsx
+++ components/AnimatedHeroSection/index.tsx
@@ -1,9 +1,44 @@
-const AnimatedHeroSection = ({ device1, device2, device3 }) => {
+const AnimatedHeroSection = ({
+  time1,
+  time2,
+  time3,
+  device1,
+  device2,
+  device3
+}) => {
   return (
     <div className="animatedHeroSection">
-      <div className="item1">{device1}</div>
-      <div className="item2">{device2}</div>
-      <div className="item3">{device3}</div>
+      <div
+        className="item1"
+        style={{
+          animation: `shiftRight1 ${time1}s`,
+          animationTimingFunction: "cubic-bezier(0.15, 0.72, 0.58, 0.99)",
+          animationFillMode: "forwards"
+        }}
+      >
+        {device1}
+      </div>
+      <div
+        className="item2"
+        style={{
+          animation: `shiftRight2 ${time2}s`,
+          animationTimingFunction: "cubic-bezier(0.15, 0.72, 0.58, 0.99)",
+          animationFillMode: "forwards"
+        }}
+      >
+        {device2}
+      </div>
+      <div
+        className="item3"
+        style={{
+          animation: `shiftRight3 ${time3}s`,
+          animationTimingFunction: "cubic-bezier(0.15, 0.72, 0.58, 0.99)",
+          animationFillMode: "forwards"
+        }}
+      >
+        {device3}
+      </div>
+      <div className="shadow"></div>
       <div className="triangle"></div>
     </div>
   );
